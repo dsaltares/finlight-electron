58e76a5fcc34c2250d3060a36f31ab6553c14371
David Saltares <david.saltares@gmail.com>
2024-09-17 18:25:37 +0300

    fix: make static assets loadable in prod

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,17 +2,13 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 import Routes from '@lib/routes';
+import logoUrl from './logo-no-text.svg';
 
 export default function Logo() {
   return (
     <Link to={Routes.home} style={{ textDecoration: 'none' }}>
       <Stack alignItems="center" gap={0.1}>
-        <img
-          src="./resources/logo-no-text.svg"
-          alt="logo"
-          width={100}
-          height={90}
-        />
+        <img src={logoUrl} alt="logo" width={100} height={90} />
         <Typography color="primary" fontWeight="bold" sx={{ fontSize: 24 }}>
           FINLIGHT
         </Typography>
