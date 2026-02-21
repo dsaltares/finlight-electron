5e58431c6ec761b3560592c50c8d460f99a53b99
David Saltares <david.saltares@gmail.com>
2026-02-21 12:51:42 +0200

    chore: dashboard redirection

--- next/app/dashboard/page.tsx
+++ next/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
 
 export default function DashboardPage() {
-  return <div className="flex flex-col gap-4 h-full">Hello</div>;
+  redirect('/dashboard/accounts');
 }
